0cd94ca39e202977cc8a27f5c7c8e86592a3f97f
wangping <wangping@heydaycn.com>
2018-08-06 10:23:58 +0800

    更改线上接口为https

--- src/config/host.js
+++ src/config/host.js
@@ -3,9 +3,9 @@
 const NODE_ENV = process.env.NODE_ENV;
 if (NODE_ENV === 'development') {
     // module.exports = `http://120.77.215.34:9001`
-    module.exports = `http://zkapi.shuzizhongwen.com`
+    module.exports = `https://zkapi.shuzizhongwen.com`
 } else if (NODE_ENV === 'test') {
     module.exports = `http://120.77.215.34:9001`
 } else {
-    module.exports = `http://zkapi.shuzizhongwen.com`
+    module.exports = `https://zkapi.shuzizhongwen.com`
 }
